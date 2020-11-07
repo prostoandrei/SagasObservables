18babb024bdc7917c3488132fd6164412bb36c31
Andrey Vaganov <aevaganov@avito.ru>
2020-11-07 18:49:03 +0300

    cleaned

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,5 @@ export const Api = {
     fetchUser: () => {
         return fetch('https://jsonplaceholder.typicode.com/todos/1')
             .then(response => response.json());
-            // .then(json => console.log(json))
-        // return new Promise((resolve, reject) => {
-        //     setTimeout(() => {
-        //         resolve(require('./data.json'));
-        //     }, 3000);
-        // })
     }
 }
